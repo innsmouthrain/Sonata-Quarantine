5c1b247f807d155b2999cf0278b8949c3587665c
Love von Melen Ekman <innsmouth.rain@gmail.com>
2018-09-27 10:55:19 +0200

    upgrading babel v1

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import ReactDOM from 'react-dom';
 // main app
 import App from '../containers/App';
 
-// ReactDOM.render(<App />, document.getElementById('app'))
-window.renderApp = function(id){
-  ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById(id)
-  );
-};
+ReactDOM.render(<App />, document.getElementById('app'))
+// window.renderApp = function(id){
+//   ReactDOM.render(
+//     <h1>Hello, world!</h1>,
+//     document.getElementById(id)
+//   );
+// };
